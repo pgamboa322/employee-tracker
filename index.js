d6407aac5334e92aee94adcfae2b7ae236e6cbd1
pgamboa322 <pgamboa322@gmail.com>
2022-08-18 20:45:33 -0500

    added in some functionality for inquirer prompts.

---
+++ index.js
@@ -0,0 +1,242 @@
+const mySQL = require("mysql2");
+const inquirer = require("inquirer");
+const connection = require("./db/connection");
+const utils = require("util");
+const table = require("console.table");
+
+const departments = [];
+
+function employeeTracker() {
+    inquirer
+      .prompt({
+        type: "list",
+        name: "choices",
+        message: "What would you like to do?",
+        choices: [
+          "View team departments",
+          "View team roles",
+          "View team employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update current employee role",
+          "Quit",
+        ],
+      })
+      .then((res) => {
+        switch (res.choices) {
+          case "View team departments":
+            viewDepts();
+            break;
+  
+          case "View team roles":
+            viewRoles();
+            break;
+  
+          case "View team employees":
+            viewEmps();
+            break;
+  
+          case "Add a department":
+            addDepartment();
+            break;
+  
+          case "Add a role":
+            addRole();
+            break;
+  
+          case "Add an employee":
+            addEmployee();
+            break;
+  
+          case "Quit":
+            console.log("Done tracking your team?");
+            connection.end();
+            break;
+        }
+      });
+  }
+
+  // Function to view departments
+
+function viewDepts() {
+    connection.query("SELECT * FROM department", (err, data) => {
+      if (err) throw err;
+      console.table(data);
+      employeeTracker();
+    });
+  }
+  
+  // Function to view roles
+  
+  function viewRoles() {
+    connection.query("SELECT * FROM employee", (err, data) => {
+      if (err) throw err;
+      console.table(data);
+      employeeTracker();
+    });
+  }
+  
+  // Function to view employees
+  
+  function viewEmps() {
+    connection.query("SELECT * FROM employee", (err, data) => {
+      if (err) throw err;
+    //   console.table(employees);
+      console.table(data);
+      employeeTracker();
+    });
+  }
+
+  // Adding in a new department
+function addDepartment() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "department",
+          message: "Add a new department: ",
+          validate: (responses) => {
+            if (responses) {
+              return true;
+            } else {
+              console.log("Input new department name.");
+            }
+          },
+        },
+      ])
+      .then((responses) => {
+        connection.query(
+          "INSERT INTO department SET ?",
+          {
+            name: responses.department,
+          },
+          (error) => {
+            if (error) throw error;
+            console.log(`New Department ${responses.department} added!`);
+            employeeTracker();
+          }
+        );
+      });
+  }
+  
+  // Function to add an employee
+  function addEmployee() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "first_name",
+          message: "Input employee's first name",
+        },
+  
+        {
+          type: "input",
+          name: "last_name",
+          message: "Input employee's last name",
+        },
+  
+        {
+          type: "input",
+          name: "role_id",
+          message: "Input Employee's role ID",
+        },
+  
+        {
+          type: "input",
+          name: "manager_id",
+          message: "Input Employee's manager id",
+        },
+      ])
+      .then((responses) => {
+        connection.query(
+          "INSERT INTO employee SET ?",
+          {
+            first_name: responses.first_name,
+            last_name: responses.last_name,
+            role_id: responses.role_id,
+            manager_id: responses.manager_id,
+          },
+          (error) => {
+            if (error) throw error;
+            console.log(`New employee ${responses.employee} added!`);
+            employeeTracker();
+          }
+        );
+      });
+    const selectRole = "SELECT * FROM employee, role";
+    connection.query(selectRole, (error, results) => {
+      if (error) throw error;
+      console.table(results);
+    });
+  }
+  
+  
+  // Function to add new role
+
+function addRole() {
+    console.log("new role");
+    const selectDepartment = deptId.map(({ id, name }) => ({
+      name: name,
+      value: id,
+    }));
+  
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "role",
+          message: "Input title of new role",
+          validate: (responses) => {
+            if (responses) {
+              return true;
+            } else {
+              console.log("Title of role is needed");
+            }
+          },
+        },
+  
+        {
+          type: "input",
+          name: "salary",
+          message: "Input salary of new role",
+          validate: (responses) => {
+            if (responses) {
+              return true;
+            } else {
+              console.log("Salary required!");
+            }
+          },
+        },
+  
+        {
+          type: "list",
+          name: "department",
+          message: "Select the department this role belongs to:",
+          choices: selectDepartment,
+          validate: (responses) => {
+            if (responses) {
+              return true;
+            } else {
+              console.log("error");
+            }
+          },
+        },
+      ])
+      .then((responses) => {
+        connection.query(
+          `INSERT INTO role SET ?`,
+          {
+            title: responses.role,
+            salary: responses.salary,
+            department_id: responses.department,
+          },
+          (error) => {
+            if (error) throw error;
+            console.log(`New role ${responses.role} added!`);
+            viewRoles();
+            employeeTracker();
+          }
+        );
+      });
+  }
