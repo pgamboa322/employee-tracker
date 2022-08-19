3cbd82f83bef3a870f6261887952f1c4f84a75aa
pgamboa322 <pgamboa322@gmail.com>
2022-08-18 20:13:40 -0500

    set up folder structure and made connection to mysql

---
+++ db/connection.js
@@ -0,0 +1,18 @@
+const mySQL = require('mysql2');
+require('dotenv').config();
+
+const connection = mySQL.createConnection(
+    {
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        database: "team_tracker_db",
+    },
+);
+
+ connection.connect((error) => {
+     console.log(error);
+     if(error) throw error;
+ });
+
+ module.exports = connection
